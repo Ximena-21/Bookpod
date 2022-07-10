357b5ac0fc617f25d98bedca95670f4b8fc27765
Ximena-21 <ximenanzola.21@gmail.com>
2022-07-10 17:21:54 -0500

    initial configuration sass to css

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
+// import validator from './validator.js';
 const headerBtn = document.querySelector('.headerBtn')
 const headerMenu = document.querySelector('.headerMenu')
-headerBtn.addEventListener('click', hideMenu)
+
+
+headerBtn.addEventListener('click', toggleMenu)
 
 
 //establecer funcion que mediante el cambio de estilos a la barra de menu lo esconda y lo muestre
-    //tambien podria usar un toggle, y haciendolo con clases
-function hideMenu () {
-
-    if(headerMenu.style.display != 'block') {
-        headerMenu.style.display = 'block'
-    } else {
-        headerMenu.style.display = 'none'
-    }
+//tambien podria usar un toggle, y haciendolo con clases
+function toggleMenu () {
+    // if(headerMenu.style.display != 'block') {
+    //     headerMenu.style.display = 'block'
+    // } else {
+    //     headerMenu.style.display = 'none'
+    // }
+    headerMenu.classList.toggle('headerMenu--hidden')
 }
 
-
-
-
-
-
-import validator from './validator.js';
-
-console.log(validator);
